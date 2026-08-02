18b4e78eaf9cd29420e5122a56f9a20c0c8aecad
rji286132-commits <rji286132@gmail.com>
2026-08-02 15:46:55 -0700

    Update route.ts

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { supabase } from '@/lib/db/supabase';
+import { supabase } from '../../../../lib/db/supabase'
 import bcrypt from 'bcryptjs';
 
 export async function POST(req: NextRequest) {
