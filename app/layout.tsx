9c7abad7ad5e789eb4f624604949e47c360d2c4d
rji286132-commits <rji286132@gmail.com>
2026-08-02 19:07:47 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body style={{margin:0}}>{children}</body>
+      <body style={{ margin: 0 }}>{children}</body>
     </html>
   );
 }
